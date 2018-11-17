a22a2534eb8217b5bc7b7b0bab0ea4daf6f723d9
JustSri <harshachel@gmail.com>
2018-11-17 14:10:51 +0530

    Fixed the grammar of the comment

--- helloworld.c
+++ helloworld.c
@@ -4,4 +4,4 @@ int main(void)
   printf("Hello world\n");
   return 0;
 }
-/*I added a comment, do what you want about it, you won't be. You are truly stupid*/
+/*I added a comment, do what you want about it, you won't be able to. You are truly stupid*/
